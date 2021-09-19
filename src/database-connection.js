e027c8293d860322f59f1a509a9205c68cfff883
dilekyoruk <dilek.yoruk@outlook.com>
2021-09-19 23:35:30 +0200

    Add mongodb connection string

--- src/database-connection.js
+++ src/database-connection.js
@@ -3,29 +3,18 @@ const mongoose = require('mongoose');
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DATABASE;
+let connectionString = process.env.MONGODB_CONNECTION_STRING;
+
+if (!connectionString) {
+  connectionString = `mongodb+srv://${username}:${password}@cluster0.knvjh.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+}
+
+mongoose.set('debug', true);
 
 mongoose
-  .connect(`mongodb+srv://${username}:${password}@cluster0.fsakt.mongodb.net/${dbName}?retryWrites=true&w=majority`, {
+  .connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log('connection established'))
   .catch(console.log);
-
-// const Panda = mongoose.model('Panda', { name: String, age: Number });
-
-// const panda = new Panda({ name: 'tim', age: 33 });
-
-// panda.save().then(() => console.log(`We have a new panda, ${panda.name}!`));
-
-/* async function main() {
-  const pandas = await Panda.find({ age: { $gte: 21 } });
-  console.log(pandas);
-}
- */
-/* async function main() {
-  const pandas = await Panda.find({ age: { $gte: 18, $lte:21 } , name: 'haluk' });
-  console.log(pandas);
-} */
-
-// main();
