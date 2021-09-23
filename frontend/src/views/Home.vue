a8477941b86cc6fc500d87260e26dbe07e515dcd
dilekyoruk <dilek.yoruk@outlook.com>
2021-09-23 10:13:36 +0200

    Create frontend folder and install vue

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .home
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
+    div(v-for="user in users") {{ user.name }} has {{ user.savedPhotos.length }} photos
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async created() {
+    const usersRequest = await axios.get('/api/users');
+
+    this.users = usersRequest.data;
+  },
+};
+</script>
