e9a0eba4f9f9640accdffb06c04f68340366dc34
dilekyoruk <dilek.yoruk@outlook.com>
2021-09-29 00:53:44 +0200

    Add photos,ratings,followers,available times to card

--- frontend/src/components/tattoo-artist-card.vue
+++ frontend/src/components/tattoo-artist-card.vue
@@ -8,6 +8,11 @@ export default {
 <template lang="pug">
 .box
     h1 {{tattooArtist.name}} ({{tattooArtist.location}})
+    p Photos: {{tattooArtist.photos[0].description}}
+    img(:src= "'/api/tattoo-artists/photos/' + tattooArtist.photos[0].filename" width='130px' height='130px')
+    p Ratings: {{tattooArtist.ratings}}
+    p Followers: {{tattooArtist.followers}}
+    p Available Times: {{tattooArtist.availableTimes}}
 </template>
 
 <style lang="scss" scoped>
