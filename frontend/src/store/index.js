423854c93082624790b58e0efe9b7ea102f53b8c
dilekyoruk <dilek.yoruk@outlook.com>
2021-10-04 22:42:28 +0200

    Create a store for the user

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,10 +4,28 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
+const mutations = {
+  INCREMENT_COUNT: 'increment count',
+  SET_USER: 'set user',
+};
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    user: null,
+  },
+  mutations: {
+    [mutations.INCREMENT_COUNT](state) {
+      state.count++;
+    },
+    [mutations.SET_USER](state, user) {
+      state.user = user;
+    },
+  },
   actions: {
+    incrementCount({ commit }) {
+      commit(mutations.INCREMENT_COUNT);
+    },
     async fetchUser(store, id) {
       const usersRequest = await axios.get(`/api/users/${id}`);
       return usersRequest.data;
@@ -15,6 +33,25 @@ export default new Vuex.Store({
     async fetchUsers() {
       const usersRequest = await axios.get('/api/users');
       return usersRequest.data;
+    },
+    async fetchSession({ commit }) {
+      const user = await axios.get('/api/account/session');
+      commit(mutations.SET_USER, user.data || null);
+    },
+    async login({ commit }, credentials) {
+      try {
+        const user = await axios.post('/api/account/session', credentials);
+        commit(mutations.SET_USER, user.data);
+      } catch (e) {
+        throw e;
+      }
+    },
+    async register(store, user) {
+      return axios.post('/api/account', user);
+    },
+    async logout({ commit }) {
+      await axios.delete('/api/account/session');
+      commit(mutations.SET_USER, null);
     },
     async fetchTattooArtists() {
       const tattooArtistsRequest = await axios.get('/api/tattoo-artists');
@@ -31,3 +68,8 @@ export default new Vuex.Store({
   },
   modules: {},
 });
+
+export default async function init() {
+  await store.dispatch('fetchSession');
+  return store;
+}
