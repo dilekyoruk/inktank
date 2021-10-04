10bde95d205d506f7fc07e30db8b5c5969dcd158
dilekyoruk <dilek.yoruk@outlook.com>
2021-10-04 22:43:57 +0200

    Add the login and register into the router/index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,42 +2,69 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import UserList from '../views/user-list.vue';
 import TattooArtistList from '../views/tattoo-artist-list.vue';
+import Login from '../views/login.vue';
+import Register from '../views/register.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'UserList',
-    component: UserList,
-  },
-  {
-    path: '/users/:id',
-    name: 'UserDetail',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/user-detail.vue'),
-  },
-  {
-    path: '/tattoo-artists',
-    name: 'TattooArtistList',
-    component: TattooArtistList,
-  },
-  {
-    path: '/tattoo-artists/:id',
-    name: 'TattooArtistDetail',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/tattoo-artist-detail.vue'),
-  },
-];
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
-});
-
-export default router;
+export default function init(store) {
+  return new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+      {
+        path: '/',
+        name: 'UserList',
+        component: UserList,
+      },
+      {
+        path: '/users/:id',
+        name: 'UserDetail',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/user-detail.vue'),
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: Register,
+        beforeEnter(to, from, next) {
+          if (store.state.user) return next('/profile');
+          return next();
+        },
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        beforeEnter(to, from, next) {
+          if (store.state.user) return next('/profile');
+          return next();
+        },
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        component: UserList,
+        beforeEnter(to, from, next) {
+          if (!store.state.user) return next('/login');
+          return next();
+        },
+      },
+      {
+        path: '/tattoo-artists',
+        name: 'TattooArtistList',
+        component: TattooArtistList,
+      },
+      {
+        path: '/tattoo-artists/:id',
+        name: 'TattooArtistDetail',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/tattoo-artist-detail.vue'),
+      },
+    ],
+  });
+}
